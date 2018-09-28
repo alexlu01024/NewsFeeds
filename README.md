b5dc52ec430b8cbe6048cf6812d970087797a4c9
alexlu01024 <alex.lu01024@gmail.com>
2018-09-28 16:52:20 -0400

    new deploy

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NewsFeeds
+This project was generated with [React](https://reactjs.org/) version 15.3.
+
+## Development server
+Run `webpack-dev-server` for a dev server. Navigate to `http://localhost:8080/`
+
+
+
+
+
---
+++ src/js/components/mobile_news_details.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Row, Col, BackTop} from 'antd';
+import MobileHeader from './mobile_header';
+import MobileFooter from './mobile_footer';
+import CommonComments from './common_comments';
+export default class MobileNewsDetails extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			newsItem: ''
+		};
+	};
+	componentDidMount() {
+		var myFetchOptions = {
+			method: 'GET'
+		};
+		fetch("" + this.props.match.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+			this.setState({newsItem: json});
+			document.title = this.state.newsItem.title + " - NewsFeeds | Base on React";
+		})
+	};
+	createMarkup() {
+		return {__html: this.state.newsItem.pagecontent};
+	};
+	render() {
+		return (
+			<div id="mobileDetailsContainer">
+				<MobileHeader></MobileHeader>
+				<div class="ucmobileList">
+					<Row>
+						<Col span={24} className="container">
+							<div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+							<hr/>
+							<CommonComments uniquekey={this.props.match.params.uniquekey}/>
+						</Col>
+					</Row>
+					<MobileFooter></MobileFooter>
+					<BackTop/></div>
+			</div>
+		);
+	};
+}
